524df7f5cb1954cbe79c6331ea067d4562768191
Rhythm <rhythmshah21@gmail.com>
2023-05-01 13:18:14 +0530

    player added

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -5,10 +5,12 @@ import MovieLogo from "../assets/homeTitle.webp";
 import { FaPlay } from "react-icons/fa";
 import {AiOutlineInfoCircle} from "react-icons/ai";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 export default function Netflix() {
   
  const [isScrolled, setIsScrolled] = useState(false);
+ const navigate = useNavigate();
  window.onscroll = () => {
   setIsScrolled(window.pageYOffset === 0 ? false : true);
   return () => (window.onscroll = null);
@@ -32,7 +34,7 @@ export default function Netflix() {
 
           </div>
           <div className="buttons flex">
-            <button className="flex j-center a-center">
+            <button className="flex j-center a-center" onClick={()=>navigate('/player')}>
               <FaPlay/>Play
             </button>
             <button className="flex j-center a-center">
